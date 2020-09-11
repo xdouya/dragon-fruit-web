ed5bafd27b4f895202b6d20cea079169fa8c2e00
caiwl <caiwenlao@sina.com>
2020-09-11 16:16:36 +0800

    修改登录页面

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,9 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'Admin-Token'
+const TokenKey = 'X-Access-Token'
+const UserName = 'username'
+const PassWord = 'password'
+const RememberMe = 'rememberMe'
 
 export function getToken() {
   return Cookies.get(TokenKey)
@@ -13,3 +16,40 @@ export function setToken(token) {
 export function removeToken() {
   return Cookies.remove(TokenKey)
 }
+
+export function getUserName() {
+  return Cookies.get(UserName)
+}
+
+export function setUserName(username) {
+  return Cookies.set(UserName, username)
+}
+
+export function removeUserName() {
+  return Cookies.remove(UserName)
+}
+
+export function getPassword() {
+  return Cookies.get(PassWord)
+}
+
+export function setPassword(password) {
+  return Cookies.set(PassWord, password)
+}
+
+export function removePassword() {
+  return Cookies.remove(PassWord)
+}
+
+export function getRememberMe() {
+  return Cookies.get(RememberMe)
+}
+
+export function setRememberMe(rememberMe) {
+  return Cookies.set(RememberMe, rememberMe)
+}
+
+export function removeRememberMe() {
+  return Cookies.remove(RememberMe)
+}
+
